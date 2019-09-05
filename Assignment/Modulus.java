e4eb1691705adee32329ec91cc39e138e5b46dd6
unknown <adejokesonoiki@gmail.com>
2019-09-05 15:18:51 +0100

    My second commit

---
+++ Assignment/Modulus.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Modulus {
+	public static void main(String[] args) {
+	
+	Scanner input = new Scanner (System.in);
+
+	System.out.print("Enter firstNumber: ")
+	int firstNumber = ("input.nextNumber();
+	
+	System.out.print("Enter secondNumber: ")
+	int secondNumber = input.nextNumber();
+	
+	if 
+
+	}
+}
---
+++ Subtraction/Subtraction.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+	public class Subtraction {
+		public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("What's your last balance: ");
+		int number1 = input.nextInt();
+
+		System.out.print("How much do you want: ");
+		int number2 = input.nextInt();
+
+		int substraction = number1 - number2;
+
+		System.out.printf("Subtraction is %d%n", Subtraction);
+		}
+	}
