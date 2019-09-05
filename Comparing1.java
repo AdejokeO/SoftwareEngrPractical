31a1eca2590c23f5b80a995088d1039f7951f408
unknown <adejokesonoiki@gmail.com>
2019-09-05 15:04:50 +0100

    My first commit

---
+++ Comparing1.java
@@ -0,0 +1,16 @@
+public class Comparing1 {
+	public static void main(String[] args) {
+		
+	int x=2;
+	int y=3;
+	
+	System.out.print ("2 =%d%n, 2");
+	
+	System.out.printf("value of %d + %d is %d%n", 2, 2, (2 + 2));
+
+	System.out.printf("2=");
+
+	System.out.printf("%d =%d%n", (2 + 3), (3 + 2));
+		
+	}
+}
---
+++ Deafness.java
@@ -0,0 +1,6 @@
+public class Deafness {
+	public static void main(String[] args){
+	System.out.printf("All categories of Deafness");
+
+	}
+}
